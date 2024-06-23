ec76fa7bddb6b77257b2c40916552ec03aa75423
Joho <Little-Red-Cap@foxmail.com>
2024-06-24 06:00:35 +0800

    Create 自动更新工具链.py

---
+++ WidgetsDemo/parts/自动更新工具链.py
@@ -0,0 +1,103 @@
+
+# LLVM
+# Clang
+# CMake
+# Ninja
+# Git
+# Python
+# Node.js
+# Yarn
+# npm
+# GCC
+# GDB
+# OpenOCD
+#
+
+# Toolchains:
+# - LLVM
+# - Clang
+# - CMake
+# - Ninja
+# - Git
+# - Python
+# - Node.js
+# - Yarn
+# - npm
+# - GCC
+# - GDB
+
+# Update LLVM:
+# - Download the latest release from https://github.com/llvm/llvm-project/releases
+# - Extract the archive to a directory of your choice
+# - Rename the directory to `llvm`
+# - Update the `LLVM_DIR` environment variable to point to the new directory
+
+
+# Update Clang:
+# - Download the latest release from https://github.com/llvm/llvm-project/releases
+# - Extract the archive to a directory of your choice
+# - Rename the directory to `clang`
+# - Update the `CLANG_DIR` environment variable to point to the new directory
+
+
+# Update CMake:
+# - Download the latest release from https://github.com/Kitware/CMake/releases
+# - Extract the archive to a directory of your choice
+# - Rename the directory to `cmake`
+# - Update the `CMAKE_DIR` environment variable to point to the new directory
+
+
+# Update Ninja:
+# - Download the latest release from https://github.com/ninja-build/ninja/releases
+# - Extract the archive to a directory of your choice
+# - Rename the directory to `ninja`
+# - Update the `NINJA_DIR` environment variable to point to the new directory
+
+
+# Update Git:
+# - Download the latest release from https://git-scm.com/downloads
+# - Extract the archive to a directory of your choice
+# - Rename the directory to `git`
+# - Update the `GIT_DIR` environment variable to point to the new directory
+
+
+# Update Python:
+# - Download the latest release from https://www.python.org/downloads/
+# - Extract the archive to a directory of your choice
+# - Rename the directory to `python`
+# - Update the `PYTHON_DIR` environment variable to point to the new directory
+
+
+# Update Node.js:
+# - Download the latest release from https://nodejs.org/en/download/
+# - Extract the archive to a directory of your choice
+# - Rename the directory to `node`
+# - Update the `NODE_DIR` environment variable to point to the new directory
+
+
+# Update Yarn:
+# - Download the latest release from https://classic.yarnpkg.com/en/docs/install
+# - Extract the archive to a directory of your choice
+# - Rename the directory to `yarn`
+# - Update the `YARN_DIR` environment variable to point to the new directory
+
+
+# Update npm:
+# - Download the latest release from https://nodejs.org/en/download/
+# - Extract the archive to a directory of your choice
+# - Rename the directory to `npm`
+# - Update the `NPM_DIR` environment variable to point to the new directory
+
+
+# Update GCC:
+# - Download the latest release from https://gcc.gnu.org/releases.html
+# - Extract the archive to a directory of your choice
+# - Rename the directory to `gcc`
+# - Update the `GCC_DIR` environment variable to point to the new directory
+
+
+# Update GDB:
+# - Download the latest release from https://www.gnu.org/software/gdb/
+# - Extract the archive to a directory of your choice
+# - Rename the directory to `gdb`
+# - Update the `GDB_DIR` environment variable to point to the new directory
